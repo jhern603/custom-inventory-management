4fb424bed124eecba4afb2040189292da386b4a3
jhern603 <jhern603@gmail.com>
2023-01-04 16:44:30 -0500

    Code refactoring

--- components/fields/SelectField.js
+++ components/fields/SelectField.js
@@ -2,11 +2,12 @@ import { useState } from 'react';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import { InputLabel } from '@mui/material';
-const SelectField = ({ list, field }) => {
+const SelectField = ({ list, field, eventHandler }) => {
   const [selectState, setSelectState] = useState(list[0]);
   const handleChange = (e) => {
     setSelectState(e.target.value);
   };
+
   return (
     <>
       <br />
--- pages/user/manageUsers.js
+++ pages/user/manageUsers.js
@@ -4,7 +4,7 @@ import { useState } from 'react';
 export default function Index() {
   const [docRetrieved, setDocRetrieved] = useState(false);
   const [users, setUsers] = useState([]);
-
+//BUG: Changes to name will override all user's names.
   if (!docRetrieved) {
     getdoc().then((doc) => {
       setUsers(doc);
